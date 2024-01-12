f3dfb81bfdc50fdfdabba5bcad4d840b7ed5887f
e-V01 <91158407+e-V01@users.noreply.github.com>
2024-01-12 10:28:02 +0200

    Initial Commit

---
+++ Fructus/FructusApp.swift
@@ -0,0 +1,17 @@
+//
+//  FructusApp.swift
+//  Fructus
+//
+//  Created by Y K on 12.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct FructusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
