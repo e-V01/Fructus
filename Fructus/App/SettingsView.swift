2f216137f5220ca297386199f1980209bef86847
e-V01 <91158407+e-V01@users.noreply.github.com>
2024-01-22 14:04:11 +0200

    2nd groupbox added to reset the whole app settings with the help of appstorage

--- Fructus/App/SettingsView.swift
+++ Fructus/App/SettingsView.swift
@@ -9,6 +9,8 @@ import SwiftUI
 
 struct SettingsView: View {
     @Environment(\.presentationMode) var presentationMode
+    @AppStorage("isOnboarding") var isOnboarding: Bool = false
+    
     var body: some View {
         NavigationView {
             ScrollView(.vertical, showsIndicators: false) {
@@ -32,6 +34,24 @@ struct SettingsView: View {
                             .padding(.vertical, 4)
                     }
                     //MARK: - Section 2
+                    GroupBox {
+                        
+                    } label: {
+                        
+                        SettingsLabelView(labelText: "Customization", labelImage: "paintbrush")
+                        Divider()
+                            .padding(.vertical, 4)
+                        Text("If you wish, you can restart the application by toggle the switch in this box. That way it starts the onboarding process and you will see the welcome screen again.")
+                            .padding(.vertical, 8)
+                            .frame(minHeight: 60)
+                            .layoutPriority(1)
+                            .font(.footnote)
+                            .multilineTextAlignment(.leading)
+                        
+                        Toggle(isOn: $isOnboarding) {
+                            Text("Restart".uppercased())
+                        }
+                    }
 
                     //MARK: - Section 3
                     GroupBox {
